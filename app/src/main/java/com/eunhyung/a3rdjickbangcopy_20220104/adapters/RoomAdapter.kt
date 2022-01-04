e7c1a7df43bb19ce19e0ddd442ac285547dfb23e
전은형 <ooooo5h91@gmail.com>
2022-01-04 21:41:31 +0900

    RoomAdapter 기초 작업 완료 - onBind 뺴고

--- app/src/main/java/com/eunhyung/a3rdjickbangcopy_20220104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/eunhyung/a3rdjickbangcopy_20220104/adapters/RoomAdapter.kt
@@ -1,17 +1,27 @@
 package com.eunhyung.a3rdjickbangcopy_20220104.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.eunhyung.a3rdjickbangcopy_20220104.R
+import com.eunhyung.a3rdjickbangcopy_20220104.models.RoomData
 
-class RoomAdapter : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
+class RoomAdapter(
+    val mContext : Context,
+    val mList : List<RoomData>
+) : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(row : View) : RecyclerView.ViewHolder(row) {
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
+        val row = LayoutInflater.from(mContext).inflate(R.layout.room_list_item, parent, false)
+        return MyViewHolder(row)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
@@ -19,6 +29,6 @@ class RoomAdapter : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-
+        return mList.size
     }
 }
