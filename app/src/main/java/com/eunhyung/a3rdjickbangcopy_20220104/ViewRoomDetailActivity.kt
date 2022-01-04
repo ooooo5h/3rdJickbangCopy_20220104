f3d3d9d33f69ff80bc818d2714803d0889620bbf
전은형 <ooooo5h91@gmail.com>
2022-01-04 22:28:21 +0900

    방 상세보기 화면에서, 보내준 RoomData 받기

--- app/src/main/java/com/eunhyung/a3rdjickbangcopy_20220104/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/eunhyung/a3rdjickbangcopy_20220104/ViewRoomDetailActivity.kt
@@ -2,10 +2,15 @@ package com.eunhyung.a3rdjickbangcopy_20220104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eunhyung.a3rdjickbangcopy_20220104.models.RoomData
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val roomData = intent.getSerializableExtra("room") as RoomData
+
+
     }
 }
