988fd689683aba6ce26b9b7dad9d422853c129cf
전은형 <ooooo5h91@gmail.com>
2022-01-04 22:14:20 +0900

    방 상세보기 화면 UI 구현 완료

---
+++ app/src/main/java/com/eunhyung/a3rdjickbangcopy_20220104/ViewRoomDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.eunhyung.a3rdjickbangcopy_20220104
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+    }
+}
