46fba8e8bb248d38d3afdf94e61ba81ac79a55fe
전은형 <ooooo5h91@gmail.com>
2022-01-04 22:30:35 +0900

    받아낸 실제 방 데이터를 보여주기

--- app/src/main/java/com/eunhyung/a3rdjickbangcopy_20220104/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/eunhyung/a3rdjickbangcopy_20220104/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.eunhyung.a3rdjickbangcopy_20220104
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.eunhyung.a3rdjickbangcopy_20220104.models.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,11 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_room_detail)
 
         val roomData = intent.getSerializableExtra("room") as RoomData
+
+        txtPrice.text = roomData.getFormattedPrice()
+        txtAddress.text = roomData.address
+        txtFloor.text = roomData.getFormattedFloor()
+        txtDescription.text = roomData.description
 
 
     }
